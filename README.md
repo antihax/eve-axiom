5bd59c9b45e3b7ada50729f1ae8473a9c035b9a2
antihax <antihax@gmail.com>
2018-07-18 16:55:37 -0700

    Add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# eve-axiom
+
+Provides a killmail to attribute service to resolve dogma into a json output of a fittings capability.
+
+## compilation
+
+* Compile [libdogma](https://github.com/antihax/libdogma) and place it in the necessary paths for cgo to find.
+* Build the cmd directory.
+
+## operation
+
+POST ESI formatted JSON killmail at the :3005/killmail endpoint and receive a JSON response.
+The :3000 port has prometheus stats and golang pprof information. This port should not be exposed, please protect it.
+
--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/antihax/eve-axiom/axiom"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("eve-axiom: ")
+
+	axiom := axiom.NewAxiom()
+	go axiom.RunServer()
+
+	// Run metrics
+	http.Handle("/metrics", promhttp.Handler())
+
+	go log.Fatalln(http.ListenAndServe(":3000", nil))
+
+	// Handle SIGINT and SIGTERM.
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
 }
